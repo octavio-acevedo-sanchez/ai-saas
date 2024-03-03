b32176611949c8f1bc964cd8249365b9135fdf0a
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-03-02 20:56:11 -0500

    Added Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from '@clerk/nextjs';
-
 export default function DashboardPage(): React.ReactNode {
 	return (
 		<div>
 			<p>Dashboard Page</p>
-			<UserButton afterSignOutUrl='/' />
 		</div>
 	);
 }
