8315f0da60e524e30763916bc2f4140aee751c92
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-03-03 21:51:45 -0500

    Added Conversation AI API (OpenAI)

---
+++ components/user-avatar.tsx
@@ -0,0 +1,17 @@
+import { useUser } from '@clerk/nextjs';
+import { Avatar } from '@/components/ui/avatar';
+import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
+
+export const UserAvatar = (): React.ReactNode => {
+	const { user } = useUser();
+
+	return (
+		<Avatar className='h-8 w-8'>
+			<AvatarImage src={user?.imageUrl} width={80} />
+			<AvatarFallback>
+				{user?.firstName?.charAt(0)}
+				{user?.lastName?.charAt(0)}
+			</AvatarFallback>
+		</Avatar>
+	);
+};
