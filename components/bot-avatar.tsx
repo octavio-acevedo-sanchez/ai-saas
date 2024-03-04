59959003445f23d5cd02bab41a42f6d5e7777661
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-03-04 17:51:27 -0500

    Added Code Generation AI (OpenAI)

--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarImage } from '@radix-ui/react-avatar';
 
 export const BotAvatar = (): React.ReactNode => {
 	return (
-		<Avatar className='h-8 w-8'>
+		<Avatar className='h-10 w-10'>
 			<AvatarImage className='p-1' src='/logo.png' width={80} />
 		</Avatar>
 	);
--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -6,7 +6,7 @@ export const UserAvatar = (): React.ReactNode => {
 	const { user } = useUser();
 
 	return (
-		<Avatar className='h-8 w-8'>
+		<Avatar className='h-10 w-10'>
 			<AvatarImage src={user?.imageUrl} width={80} />
 			<AvatarFallback>
 				{user?.firstName?.charAt(0)}
