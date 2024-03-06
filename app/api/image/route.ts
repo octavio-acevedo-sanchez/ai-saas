232cff2aa31eeaf00e3ba8a9e667fcba3e0831b5
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-03-05 21:49:04 -0500

    Added Image Generation AI (OpenAI)

---
+++ app/api/image/route.ts
@@ -0,0 +1,42 @@
+import { OpenAI } from 'openai';
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+export async function POST(req: Request): Promise<NextResponse> {
+	try {
+		const { userId } = auth();
+		const body = await req.json();
+		const { prompt, amount = 1, resolution = '512x512' } = body;
+
+		if (!userId) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		if (!prompt) {
+			return new NextResponse('Prompt is required', { status: 400 });
+		}
+
+		if (!amount) {
+			return new NextResponse('Amount is required', { status: 400 });
+		}
+
+		if (!resolution) {
+			return new NextResponse('Resolution is required', { status: 400 });
+		}
+
+		const response = await openai.images.generate({
+			prompt,
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+			n: parseInt(amount, 10),
+			size: resolution
+		});
+		console.log(response);
+
+		return NextResponse.json(response.data);
+	} catch (error) {
+		console.log('[IMAGE_ERROR]', error);
+		return new NextResponse('Internal error', { status: 500 });
+	}
+}
